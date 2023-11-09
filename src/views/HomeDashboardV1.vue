b50db1c32836b6ef1bdd1ad16f84b62fcb5a8ddc
Tonny James P. dos Reis <86257095+TonnyJames@users.noreply.github.com>
2023-11-08 23:17:38 -0300

    dash com setup ok

--- src/views/HomeDashboardV1.vue
+++ src/views/HomeDashboardV1.vue
@@ -2,8 +2,9 @@
   <v-chart class="chart" :option="option"/>
 </template>
 
-<script setup>
+<script>
 import API from "@/API";
+import * as echarts from 'echarts'
 import {use} from "echarts/core";
 import {CanvasRenderer} from "echarts/renderers";
 import {LineChart} from "echarts/charts";
@@ -16,9 +17,7 @@ import {
   DataZoomComponent,
   GridComponent
 } from "echarts/components";
-import {onBeforeMount, onMounted, ref} from "vue";
 import api from "@/config/AxiosConfig";
-
 
 use([
   CanvasRenderer,
@@ -31,95 +30,108 @@ use([
   DataZoomComponent,
 ]);
 
-// created(async () => {
-//   await fetchValores();
-// })
+export default {
+  components: {
+    VChart
+  },
+  data: () => ({
+    data: [],
+    option: {}
+  }),
 
-let valores = [];
+  created() {
+    // setTimeout(() => this.createChart(), 1000)
+    this.createChart()
+  },
 
-const resposta =  api.get("/consultar-medicoes/inicial/")
-      valores = resposta;
+  methods: {
 
-      let data = [[+new Date(), 0]]
-      for (let i = 1; i < valores.data.length; i++) {                               //numero i representa o tamanhno da amostragem
-        data.push([+new Date(valores.data[i].data), valores.data[i].valor]);
+    async createChart() {
+
+      let valores = [];
+      const resultado = await api.get("/consultar-medicoes/inicial/");
+      valores = resultado.data;
+
+      // let data = [];
+      valores.forEach(item => {
+        this.data.push([+new Date(item.data), item.valor]);
+      })
+
+      this.option = {
+        tooltip: {
+          trigger: 'axis',
+          position: function (pt) {
+            return [pt[0], '10%'];
+          }
+        },
+        title: {
+          left: 'center',
+          text: 'Decibéis'
+        },
+
+        toolbox: {
+          feature: {
+            dataZoom: {
+              yAxisIndex: 'none'
+            },
+            restore: {},
+            saveAsImage: {}
+          }
+        },
+        xAxis: {
+          type: 'time',
+          boundaryGap: false
+        },
+        yAxis: {
+          type: 'value',
+
+        },
+        dataZoom: [
+          {
+            type: 'inside',
+            start: 0,
+            end: 20
+          },
+          {
+            start: 0,
+            end: 20
+          }
+        ],
+        series: [
+          {
+            name: 'dados do mongoDb',
+            type: 'line',
+            smooth: true,
+            symbol: 'none',
+            areaStyle: {
+              color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                {
+                  offset: 1,
+                  color: 'rgba(15,255,2,1)'
+
+                },
+                {
+                  color: 'rgba(242,1,1,1)',
+                  offset: 0,
+                }
+              ])
+            },
+            data: this.data
+          }
+        ],
       }
-
-      console.log(valores)
-
-
-// console.log(data);
-
-
-// let dateBase = +new Date("2023-10-17T03:04:00.894-03:00"); // data que receberá inicio da medida
-// let segundos = 5000;                                            //passo de integração para mostradores de valor
-// let data = [[dateBase, Math.random() * 300]];
-// for (let i = 1; i < 20000; i++) {                               //numero i representa o tamanhno da amostragem
-//   let now = new Date((dateBase += segundos));
-//   data.push([+now, Math.round((Math.random() - 0.5) * 20 + data[i - 1][1])]);
-// }
-
-// console.log(data);
-
-const option = {
-
-  tooltip: {
-    trigger: 'axis',
-    position: function (pt) {
-      return [pt[0], '10%'];
-    }
-  },
-  title: {
-    left: 'center',
-    text: 'Decibéis'
-  },
-
-  toolbox: {
-    feature: {
-      dataZoom: {
-        yAxisIndex: 'none'
-      },
-      restore: {},
-      saveAsImage: {}
-    }
-  },
-  xAxis: {
-    type: 'time',
-    boundaryGap: false
-  },
-  yAxis: {
-    type: 'value',
-
-  },
-  dataZoom: [
-    {
-      type: 'inside',
-      start: 0,
-      end: 20
     },
-    {
-      start: 0,
-      end: 20
-    }
-  ],
-  series: [
-    {
-      name: 'dados do mongoDb',
-      type: 'line',
-      smooth: true,
-      symbol: 'none',
-      areaStyle: {},
-      data: data
-    }
-  ],
 
-};
+  }
+
+}
+
 
 </script>
 
 <style scoped>
 .chart {
-  height: 30vh;
-  width: 100vh;
+  height: 50vh;
+  width: 130vh;
 }
 </style>
