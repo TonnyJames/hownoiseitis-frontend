e695a9828631a68f0790312d4b34a81733689c29
Tonny James P. dos Reis <86257095+TonnyJames@users.noreply.github.com>
2023-11-14 08:43:52 -0300

    algumas mudanças no layout

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,13 @@
 
       <NavigationDraw/>
 
-    <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
+    <v-main class="d-flex align-center " style="min-height: 300px;">
       <router-view/>
     </v-main>
+
+    <v-footer class="v-theme--dark" app name="footer"> <Footer/></v-footer>
+
+
   </v-layout>
 </template>
 
@@ -15,6 +19,7 @@
 <script setup>
 import NavigationDraw from "@/components/NavigationDraw.vue";
 import Toolbar from "@/components/Toolbar.vue";
+import Footer from "@/components/FooterApp.vue";
 
 </script>
 
---
+++ src/components/FooterApp.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-footer id="footer" >
+    <v-row justify="center"  >
+      <v-col class="text-center mt-4" cols="12">
+        <a href="https://github.com/TonnyJames?tab=repositories" target="_blank">Github - code</a>
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
+
+<style scoped>
+
+#footer {
+  background-color: var(--vt-c-black);
+  color: var(--vt-c-text-dark-2);
+}
+
+</style>
